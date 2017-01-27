92e5aa19f67a3e77f4dd9c76e9d4bd018412be2e
DanSnow <dododavid006@gmail.com>
2017-01-27 20:28:02 +0800

    chore(scripts): Only update bundle.js

--- scripts/build-gh-pages.sh
+++ scripts/build-gh-pages.sh
@@ -2,14 +2,9 @@
 
 cd example &&\
 yarn run build &&\
-cp index.html .. &&\
 cd .. &&\
-git add index.html &&\
-git stash save --keep-index &&\
-rm index.html &&\
 git checkout gh-pages &&\
-git stash pop &&\
 cp example/bundle.js . &&\
-git add index.html bundle.js &&\
+git add bundle.js &&\
 git commit -m 'chore: Build'
 git co master
