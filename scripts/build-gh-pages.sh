055979a367a0be16c6a1afcd8802a3e4b34d7334
DanSnow <dododavid006@gmail.com>
2017-01-27 20:25:06 +0800

    chore(scripts): Fix merge problem

--- scripts/build-gh-pages.sh
+++ scripts/build-gh-pages.sh
@@ -2,11 +2,14 @@
 
 cd example &&\
 yarn run build &&\
-cp index.html bundle.js .. &&\
+cp index.html .. &&\
 cd .. &&\
-git add index.html bundle.js &&\
+git add index.html &&\
 git stash save --keep-index &&\
+rm index.html &&\
 git checkout gh-pages &&\
 git stash pop &&\
+cp example/bundle.js . &&\
+git add index.html bundle.js &&\
 git commit -m 'chore: Build'
 git co master
