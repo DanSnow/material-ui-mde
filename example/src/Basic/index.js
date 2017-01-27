62eb1fc5ad01322f6304c89609f39cb0fda64471
DanSnow <dododavid006@gmail.com>
2017-01-27 22:18:21 +0800

    chore: Add defaultValue

--- example/src/Basic/index.js
+++ example/src/Basic/index.js
@@ -4,12 +4,19 @@ import CardTitle from 'material-ui/Card/CardTitle'
 import CardText from 'material-ui/Card/CardText'
 import MarkdownEditor from 'material-ui-mde'
 
+const content = `\
+material-ui-mde
+===============
+
+MarkdownEditor build on material-ui
+`
+
 function Basic() {
   return (
     <Card>
       <CardTitle title='Example for material-ui-mde' />
       <CardText>
-        <MarkdownEditor />
+        <MarkdownEditor defaultValue={content} />
       </CardText>
     </Card>
   )
